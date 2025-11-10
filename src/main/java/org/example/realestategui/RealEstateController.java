4b5f57ec8dbb0461163b17a8fd44d1b2fcd985e5
SagiBeno <benosagi321@gmail.com>
2025-11-10 14:43:54 +0100

    Rename files and modify title

--- src/main/java/org/example/realestategui/RealEstateController.java
+++ src/main/java/org/example/realestategui/RealEstateController.java
@@ -3,7 +3,7 @@ package org.example.realestategui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class RealEstateController {
     @FXML
     private Label welcomeText;
 

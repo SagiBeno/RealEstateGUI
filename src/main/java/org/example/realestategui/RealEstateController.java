13818c0afa8b8d88d53af021508779b611a3c312
SagiBeno <benosagi321@gmail.com>
2025-11-11 11:28:27 +0100

    Solve task 15.

--- src/main/java/org/example/realestategui/RealEstateController.java
+++ src/main/java/org/example/realestategui/RealEstateController.java
@@ -2,9 +2,12 @@ package org.example.realestategui;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.sql.Array;
@@ -19,7 +22,12 @@ public class RealEstateController implements Initializable {
     public static ArrayList<Seller> sellers = new ArrayList<>();
     public static ArrayList<Category> categories = new ArrayList<>();
 
+    public int sellerId = 0;
+
     @FXML public ListView<String> sellersListview;
+    @FXML public Label sellerName;
+    @FXML public Label sellerPhone;
+    @FXML public Label adNumber;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -38,5 +46,34 @@ public class RealEstateController implements Initializable {
         Collections.sort(sellersName);
         ObservableList<String> sellersNameOL = FXCollections.observableList(sellersName);
         sellersListview.setItems(sellersNameOL);
+    }
+
+    public void handleSeller(MouseEvent mouseEvent) {
+        String selectedSeller = sellersListview.getSelectionModel().getSelectedItem();
+        for (Seller seller : sellers) {
+            if (seller.getName().equals(selectedSeller)) {
+                sellerId = seller.getId();
+                sellerName.setText(seller.getName());
+                sellerPhone.setText(seller.getPhone());
+            }
+        }
+    }
+
+    @FXML
+    public void handleAdLoading(ActionEvent actionEvent) {
+        System.out.println(sellerId);
+        int adQuantity = 0;
+        for (Realesate realesate : realesates) {
+            if (realesate.getSellerId() == sellerId) {
+                adQuantity++;
+            }
+        }
+
+        if (adQuantity != 0) {
+            adNumber.setText(String.valueOf(adQuantity));
+        } else {
+            adNumber.setText("Nincs hirdetés");
+        }
+
     }
 }
